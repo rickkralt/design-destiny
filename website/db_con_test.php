f329245b829f6c04d6efafcaa0c16a7f4b4a99a5
Parick <veldhuisenpatrick@gmail.com>
2018-11-13 11:01:03 +0100

    db_con file change

--- website/db_con_test.php
+++ website/db_con_test.php
@@ -1,6 +1,8 @@
 <?php
 $db_con = new SQLite3('bed_en_breakfest.db');
 $reslult  = $db_con->query('select * from kamers');
+$reslult2  = $db_con->query('select * from ExtraOpties');
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -21,7 +23,20 @@ $reslult  = $db_con->query('select * from kamers');
             echo "<tr><td>".$row->kamer_id."</td><td>".$row->kamer_personen."</td><td>".$row->kamer_naam."</td><td>".$row->kamer_prijs."</td></tr>";
         }
         ?>
+    </table><br/>
+    <hr/>
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Naam</th>
+            <th>Prijs</th>
+        </tr>
+        <?php
+        while ($row = $reslult2->fetchArray() ){
+            $row = (object) $row;
+            echo "<tr><td>".$row->extraopties_id."</td><td>".$row->extraopties_naam."</td><td>".$row->extraopties_prijs."</td></tr>";
+        }
+        ?>
     </table>
 </body>
 </html>
-
