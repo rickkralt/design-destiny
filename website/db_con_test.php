b0867f45402dfe9d200fcedb0382f79b19921364
Parick <veldhuisenpatrick@gmail.com>
2018-11-13 10:43:26 +0100

    db file save

--- website/db_con_test.php
+++ website/db_con_test.php
@@ -1,13 +1,27 @@
 <?php
-$db_con = new SQLite3('../database/bed_en_breakfest.db');
-$result = $db_con->query('select * from kamers');
-$results = $result->fetchArray();
-
-while ($row = $results){
-    echo $row[kamer_id];
-}
-
-
-
-
+$db_con = new SQLite3('bed_en_breakfest.db');
+$reslult  = $db_con->query('select * from kamers');
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dit is een test!</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Personen</th>
+            <th>Naam</th>
+            <th>Prijs</th>
+        </tr>
+        <?php
+        while ($row = $reslult->fetchArray() ){
+            $row = (object) $row;
+            echo "<tr><td>".$row->kamer_id."</td><td>".$row->kamer_personen."</td><td>".$row->kamer_naam."</td><td>".$row->kamer_prijs."</td></tr>";
+        }
+        ?>
+    </table>
+</body>
+</html>
+
