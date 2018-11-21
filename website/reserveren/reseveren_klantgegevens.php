56f27cb7ec5a58e2455609cc981ca5ad4087328c
Parick <veldhuisenpatrick@gmail.com>
2018-11-21 10:54:36 +0100

    Save - Einde dag!

--- website/reserveren/reseveren_klantgegevens.php
+++ website/reserveren/reseveren_klantgegevens.php
@@ -42,7 +42,7 @@ $ressult = $db_con->query("SELECT * FROM kamers WHERE kamer_id = '$ButtonValue'"
     <link rel="stylesheet" href="../style/style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-    <title>*TEST* Reseveren</title>
+    <title>Reseveren</title>
 </head>
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
